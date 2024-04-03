530b96ed1721f7992717eef1bf059194aea2b0a8
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-02 21:20:51 -0300

    v1.3

--- clube_livro/criacaoTabelas.sql
+++ clube_livro/criacaoTabelas.sql
@@ -8,6 +8,7 @@ CREATE TABLE tbaluno (
   nome_aluno VARCHAR(50) NOT NULL,
   telefone_aluno CHAR(9) NOT NULL,
   cidade_aluno VARCHAR(20) DEFAULT "IRATI",
+
   CONSTRAINT pk_tbaluno PRIMARY KEY(matr_aluno),
   CONSTRAINT un_rg_aluno UNIQUE(rg_aluno)
 ) ENGINE = INNODB;
@@ -16,5 +17,24 @@ DESC tbaluno;
 CREATE TABLE tbarea (
 	cod_area INT NOT NULL,
 	nome_area VARCHAR(50) NOT NULL,
+
 	CONSTRAINT pk_tbarea PRIMARY KEY(cod_area)
 ) ENGINE = INNODB;
+DESC tbarea;
+
+CREATE TABLE tblivro (
+	isbn_livro INT NOT NULL,
+	nome_livro VARCHAR(50) NOT NULL,
+	autor_livro VARCHAR(50),
+	matr_aluno INT,
+	cod_area INT,
+	
+	CONSTRAINT pk_tblivro PRIMARY KEY(isbn_livro),
+	CONSTRAINT fk_tblivro_tbaluno FOREIGN KEY(matr_aluno)
+	REFERENCES tbaluno(matr_aluno),
+	CONSTRAINT fk_tblivro_tbarea FOREIGN KEY(cod_area)
+	REFERENCES tbarea(cod_area)
+
+	ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+DESC tblivro;
