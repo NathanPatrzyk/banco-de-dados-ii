71865a290fbee715a5b1eaa07e6421eddf549400
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-08-02 17:13:33 -0300

    v12.0

--- aula-09/queries.sql
+++ aula-09/queries.sql
@@ -38,18 +38,68 @@ WHERE
 SELECT c.nome_cliente AS "Nome do Cliente"
 FROM tbcliente c
 WHERE
-    c.cod_cliente NOT IN (
+    c.cod_cliente NOT IN(
         SELECT e.cod_cliente
         FROM tbemprestimo e
     );
 
 -- Quais os nomes e categorias dos títulos que NÃO são de Terror, Suspense ou Drama?
-SELECT 
-    t.nome_titulo AS "Nome do Título",
-    c.nome_categoria AS "Nome da Categoria"
+SELECT t.nome_titulo AS "Nome do Título", c.nome_categoria AS "Nome da Categoria"
 FROM tbtitulo t
-    INNER JOIN tbcategoria c
-    ON t.cod_categoria = c.cod_categoria
+    INNER JOIN tbcategoria c ON t.cod_categoria = c.cod_categoria
 WHERE
-    c.nome_categoria
-    NOT IN("Terror", "Suspense", "Drama");
+    c.nome_categoria NOT IN("Terror", "Suspense", "Drama");
+
+-- EXISTS 1
+-- Quais os titulos que possuem o preço da classe menor que 8.00?
+SELECT t.nome_titulo AS "Nome do Título", c.preco_classe AS "Preço da Classe"
+FROM tbtitulo t
+    INNER JOIN tbclasse c ON c.cod_classe = t.cod_classe
+WHERE
+    EXISTS (
+        SELECT c.cod_classe
+        FROM tbclasse c
+        WHERE
+            c.cod_classe = t.cod_classe
+            AND c.preco_classe < 8
+    );
+
+-- EXISTS 2
+-- Quais os nomes dos filmes que possuem o nome de título Fusce?
+SELECT f.nome_filme AS "Nome do Filme"
+FROM tbfilme f
+WHERE
+    EXISTS (
+        SELECT t.cod_titulo
+        FROM tbtitulo t
+        WHERE
+            t.cod_titulo = f.cod_titulo
+            AND t.nome_titulo = "Fusce"
+    );
+
+-- NOT EXISTS 1
+-- Quais os filmes que os clientes do estado de SP NÃO emprestaram?
+SELECT f.nome_filme AS "Nome do Filme"
+FROM tbfilme f
+WHERE
+    NOT EXISTS (
+        SELECT e.cod_filme
+        FROM tbemprestimo e
+            INNER JOIN tbcliente c ON c.cod_cliente = e.cod_cliente
+        WHERE
+            e.cod_filme = f.cod_filme
+            AND c.uf_cliente = "SP"
+    );
+
+-- NOT EXISTS 2
+-- Quais os nomes dos clientes que NÃO realizaram nenhum empréstimo em 2018?
+SELECT c.nome_cliente AS "Nome do Cliente"
+FROM tbcliente c
+WHERE
+    NOT EXISTS (
+        SELECT e.data_emprestimo
+        FROM tbemprestimo e
+        WHERE
+            e.cod_cliente = c.cod_cliente
+            AND e.data_emprestimo BETWEEN "2018-01-01" AND "2018-12-31"
+    );
